937ff121aa54720c32d09565cbbca6e5129bf086
Michael McRae <michael@magmacomputing.com.au>
2022-09-16 10:40:06 +1000

    check-in serialize library

--- lib/shared/array.library.ts
+++ lib/shared/array.library.ts
@@ -1,6 +1,6 @@
 import { asString } from '@module/shared/string.library';
 import { getPath } from '@module/shared/object.library';
-import { clonify } from '@module/shared/serialize.library';
+import { cloneify } from '@module/shared/serialize.library';
 import { asType, isNumber, isDate, isIterable, isString, isBoolean, isArrayLike, nullToValue } from '@module/shared/type.library';
 
 /** Coerce value into value[], if not already value[], with optional fill Object */
@@ -16,7 +16,7 @@ export function asArray<T, K>(arr: T | Iterable<T> | ArrayLike<T> = [], fill?: K
 			return Array.from<T, K>(arr as T[], val => {
 				return args.type === 'Undefined' || val !== void 0
 					? val as unknown as K															// if no 'fill', then use val
-					: clonify(fill as K)															// clone 'fill' to create new Objects
+					: cloneify(fill as K)															// clone 'fill' to create new Objects
 			});
 
 		default:
