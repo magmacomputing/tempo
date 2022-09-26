8b7f92ef37e7d1782dd89c5614ca80f0c0a56869
Michael McRae <michael@magmacomputing.com.au>
2022-09-27 09:44:16 +1000

    Tempo.getCanonicalLocales

--- lib/shared/enum.library.ts
+++ lib/shared/enum.library.ts
@@ -2,26 +2,37 @@ import { isNumeric } from '@module/shared/number.library';
 import { isNumber } from '@module/shared/type.library';
 
 /**
- * Enums have three types: Numeric (with reverse-mapping), String and Heterogeneous (mixed Number and String) keys.  
+ * Typescript Enums have three types:  
+ * a. Numeric (with reverse-mapping),
+ * b. String and 
+ * c. Heterogeneous (mixed Number and String) keys.  
  */
 
 /** array of Enum keys */
 export const enumKeys = <T>(enumType: T) => {
-	const enumEntries = Object.entries(enumType as T & object).filter(([, val]) => isNumber(val)) as [keyof T, number][];
-	const enumKeys = Object.keys(enumType as T & object).filter(key => !isNumeric(key)) as (keyof T)[];
+	const enumEntries = Object.entries(enumType as T & object).filter(([, val]) =>
+		isNumber(val)) as [keyof T, number][];
+
+	const enumKeys = Object.keys(enumType as T & {})					// Enum keys
+		.filter(key => !isNumeric(key)) as (keyof T)[];
 
 	return (enumEntries.length === enumKeys.length)						// if Numeric Enum
 		? enumEntries
 			.sort(([, val1], [, val2]) => val1 = val2)						// sort by the number-values
-			.map(([key,]) => key)																	// and return the keys
+			.map(([key,]) => key)																	// 	and return the keys
 		: enumKeys																							// else String/Heterogeneous Enum
 }
 
-/** Count of Enum keys */
-export const enumCount = <T>(enumType: T) => enumKeys(enumType).length;
+/** count of Enum keys */
+export const enumCount = <T>(enumType: T) =>								// Enum length
+	enumKeys(enumType).length;
 
-/** Array of Enum values */
-export const enumValues = <T>(enumType: T) => enumKeys(enumType).map(key => enumType[key]);
+/** array of Enum values */
+export const enumValues = <T>(enumType: T) =>								// Enum values
+	enumKeys(enumType)
+		.map(key => enumType[key]);
 
-/** Array of Enum's [key, value] tuples */
-export const enumEntries = <T>(enumType: T) => enumKeys(enumType).map(key => [key, enumType[key]] as [keyof T, T[keyof T]]);
+/** array of Enum's [key, value] tuples */
+export const enumEntries = <T>(enumType: T) =>							// Enum entries
+	enumKeys(enumType)
+		.map(key => [key, enumType[key]] as [keyof T, T[keyof T]]);
