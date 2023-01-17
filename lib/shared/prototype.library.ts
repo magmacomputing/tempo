26cec1faa5ec8fdc6a7b71afcbfd065643145f7d
Michael McRae <michael@magmacomputing.com.au>
2023-01-17 11:59:40 +1100

    prototype.library

--- lib/shared/prototype.library.ts
+++ lib/shared/prototype.library.ts
@@ -1,201 +1,92 @@
+// @ts-nocheck
+
+import { clean, toProperCase } from '@module/shared/string.library';
 import { asArray, keyedBy, sortBy, type SortBy } from '@module/shared/array.library';
-import { toProperCase } from '@module/shared/string.library';
 
 // Prototype extensions
 // Remember to define any imports as a Function declaration (not a function expression)
 // so that they are 'hoisted' prior to extending a prototype
 
+/**
+ * extend an Object's prototype to include new method, if possible
+ */
+const patch = (proto: StringConstructor | ArrayConstructor, property: string, method: Function) => {
+	if (!proto.prototype.hasOwnProperty(property)) {
+		Object.defineProperty(proto.prototype, property, {
+			configurable: false,
+			enumerable: false,
+			writable: false,
+			value: method,
+		})
+	} else {																									// if already defined with different body, show error
+		if (clean(method.toString()) !== clean((proto.prototype as any)[property]?.toString()))
+			console.error(`${proto.name}.${property} already defined`);
+	}
+}
+
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // extend String prototype
 declare global {
 	interface String {
-		/** remove redundant spaces to a new string */
-		trimAll(pat?: RegExp): string;
+		/** remove redundant spaces to a new string */					trimAll(pat?: RegExp): string;
 
-		/** upper-case first letter of a word */
-		toProperCase(): string;
-
-		/** needed until Typescript adds declaration */
-		// replaceAll(searchValue: string | RegExp, replaceValue: string): string;
+		/** upper-case first letter of a word */								toProperCase(): string;
 	}
 }
 
-if (!String.prototype.hasOwnProperty('trimAll')) {
-	Object.defineProperty(String.prototype, 'trimAll', {
-		enumerable: false,
-		configurable: false,
-		writable: false,
-		value: function (pat: RegExp) {
-			return this
-				.replace(pat, '')
-				.replace(/\s{2,}/g, ' ')
-				.replace(/\t/g, ' ')
-				.replace(/(\r\n|\n|\r)/g, '')
-				.trim()
-		},
-	})
-}
-// else console.error('String.trimAll already defined');
-
-// if (!String.prototype.hasOwnProperty('replaceAll')) {
-// 	Object.defineProperty(String.prototype, 'replaceAll', {
-// 		enumerable: false,
-// 		configurable: false,
-// 		writable: false,
-// 		value: function (str: string, newStr: string) {
-
-// 			return isType<RegExp>(str, 'RegExp')
-// 				? this.replace(str, newStr)													// if a regex pattern
-// 				: this.replace(new RegExp(str, 'g'), newStr);				// if a string
-// 		}
-// 	})
-// }
-// else console.error('String.replaceAll already defined')
-
-if (!String.prototype.hasOwnProperty('toProperCase')) {
-	Object.defineProperty(String.prototype, 'toProperCase', {
-		enumerable: false,
-		configurable: false,
-		writable: false,
-		value: function () {
-			return toProperCase(this);
-		}
-	})
-}
-// else console.error('String.toProperCase already defined');
+patch(String, 'trimAll', function (pat?: RegExp) { return clean(this.replace(pat, '')); });
+patch(String, 'toProperCase', function () { return toProperCase(this) });
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // extend Array prototype
 declare global {
 	interface Array<T> {
-		/** reduce Array to a keyed-Object */
-		keyedBy<K extends string>(key: string): Record<K, T[]>;
+		/** reduce Array to a keyed-Object */										keyedBy<K extends string>(key: string): Record<K, T[]>;
 		keyedBy(...keys: string[]): Record<string, T[]>;
 		keyedBy<K extends string>(flatten: true, key: string): Record<K, T>;
 		keyedBy(flatten: true, ...keys: string[]): Record<string, T>;
 		keyedBy<K extends string>(flatten: false, key: string): Record<K, T[]>;
 		keyedBy(flatten: false, ...keys: string[]): Record<string, T[]>;
 
-		/** return sorted Array-of-objects */
 		/** return sorted Array-of-objects */										orderBy(keys: (string | SortBy)[]): T[];
 		/** return sorted Array-of-objects */										orderBy(...keys: (string | SortBy)[]): T[];
 		/** return sorted Array-of-objects */										sortBy(keys: (string | SortBy)[]): T[];
 		/** return sorted Array-of-objects */										sortBy(...keys: (string | SortBy)[]): T[];
 
-		/** return new Array with no repeated elements */
-		distinct(): T[];
-		/** return mapped Array with no repeated elements */
-		distinct<S>(mapfn: (value: T, index: number, array: T[]) => S, thisArg?: any): S[];
+		/** return new Array with no repeated elements */				distinct(): T[];
+		/** return mapped Array with no repeated elements */		distinct<S>(mapfn: (value: T, index: number, array: T[]) => S, thisArg?: any): S[];
 
-		/** Clear down an Array */
-		truncate(): T[];
+		/** clear down an Array */															truncate(): T[];
 
-		/** return cartesian-product of Array of Arrays */
-		cartesian(): T;
+		/** return cartesian-product of Array of Arrays */			cartesian(): T;
 		cartesian(...args: T[][]): T[];
 
-		/** Tap into an Array */
-		tap(tapfn: (value: T[]) => void, thisArg?: any): T[];
+		/** tap into an Array */																tap(tapfn: (value: T[]) => void, thisArg?: any): T[];
 	}
 }
 
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function fn(...keys: any[]) { return this.sort(sortBy(...keys)); }
+patch(Array, 'orderBy', fn);
+patch(Array, 'sortBy', fn);
+patch(Array, 'keyedBy', function (...keys: any[]) { return keyedBy(this, ...keys); });
+patch(Array, 'tap', function (fn: Function) {
+	fn(this);
+	return this;
+});
+patch(Array, 'truncate', function () {
+	this.fill(null).length = 0;
+	return this;
+})
+patch(Array, 'distinct', function (selector: (value: any, index: number, array: any[]) => []) {
+	return selector
+		? this.map(selector).distinct()
+		: asArray(new Set(this))
+})
+patch(Array, 'cartesian', function (...args: any[]) {
+	const [a, b = [], ...c] = args.length === 0 ? this : args;
+	const cartFn = (a: any[], b: any[]) => (<any[]>[]).concat(...a.map(d => b.map(e => (<any[]>[]).concat(d, e))));
 
-if (!Array.prototype.hasOwnProperty('keyedBy')) {
-	Object.defineProperty(Array.prototype, 'keyedBy', {
-		configurable: false,
-		enumerable: false,
-		writable: false,
-		value: function (...keys: any[]) {
-			return keyedBy(this, ...keys);
-		},
-	})
-}
-else console.error('Array.keyedBy already defined');
-
-if (!Array.prototype.hasOwnProperty('orderBy')) {
-	const obj = {
-		configurable: false,
-		enumerable: false,
-		writable: false,
-		value: function (...keys: any[]) {
-			return this.sort(sortBy(...keys));
-		},
-	} as PropertyDescriptor & ThisType<any[]>
-	Object.defineProperty(Array.prototype, 'orderBy', obj);
-	Object.defineProperty(Array.prototype, 'sortBy', obj);
-}
-else console.error('Array.orderBy/sortBy already defined');
-
-if (!Array.prototype.hasOwnProperty('truncate')) {
-	Object.defineProperty(Array.prototype, 'truncate', {
-		configurable: false,
-		enumerable: false,
-		writable: false,
-		value: function () {
-			this.fill(null).length = 0;
-			return this;
-		}
-	})
-}
-// else console.error('Array.truncate already defined');
-
-if (!Array.prototype.hasOwnProperty('distinct')) {
-	Object.defineProperty(Array.prototype, 'distinct', {
-		configurable: false,
-		enumerable: false,
-		writable: false,
-		value: function (selector: (value: any, index: number, array: any[]) => []) {
-			return selector
-				? this.map(selector).distinct()
-				: asArray(new Set(this))
-		}
-	})
-}
-// else console.error('Array.distinct already defined');
-
-if (!Array.prototype.hasOwnProperty('cartesian')) {
-	Object.defineProperty(Array.prototype, 'cartesian', {
-		configurable: false,
-		enumerable: false,
-		writable: false,
-		value: function (...args: any[]) {
-			const [a, b = [], ...c] = args.length === 0 ? this : args;
-			const cartFn = (a: any[], b: any[]) => (<any[]>[]).concat(...a.map(d => b.map(e => (<any[]>[]).concat(d, e))));
-
-			return b.length
-				? this.cartesian(cartFn(a, b), ...c)
-				: asArray(a || [])
-		}
-	})
-}
-// else console.error('Array.cartesian already defined');
-
-extend(Array, 'tap', (fn: Function) => { fn(this); return this; })
-if (!Array.prototype.hasOwnProperty('tap')) {
-	Object.defineProperty(Array.prototype, 'tap', {
-		configurable: false,
-		enumerable: false,
-		writable: false,
-		value: function (fn: Function) { fn(this); return this; }
-	})
-}
-// else console.error('Array.tap already defined');
-
-/**
- * extend an Object prototype to include new method
- */
-const pat = /(  |\n)/g;																			// trim multiple <space> and all <newline>
-function extend(proto: StringConstructor | ArrayConstructor, property: string, value: Function) {
-	if (!proto.prototype.hasOwnProperty(property)) {
-		Object.defineProperty(proto.prototype, property, {
-			configurable: false,
-			enumerable: false,
-			writable: false,
-			value: value.bind,
-		})
-	} else {
-		// if (value.toString().replace(pat, '') !== (proto.prototype as Record<string, Function>)[property]?.toString().replace(pat, ''))
-		// 	console.error(`${proto.name}.${property} already defined`);
-	}
-}
+	return b.length
+		? this.cartesian(cartFn(a, b), ...c)
+		: asArray(a || [])
+})
