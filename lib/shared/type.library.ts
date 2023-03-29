4aeca971112a7f4cc85332fafb497be2ec11fe21
Michael McRae <michael@magmacomputing.com.au>
2023-03-30 08:14:50 +1100

    type library

--- lib/shared/type.library.ts
+++ lib/shared/type.library.ts
@@ -98,7 +98,7 @@ export function assertNever(val: never): asserts val is never { throw new Error(
 
 /** infer T of a <T | T[]> */
 export type TValue<T> = T extends Array<infer A> ? A : NonNullable<T>;
-/** cast T as <T | T[]> */
+/** cast <T | undefined> as <T | T[]> */
 export type TValues<T> = TValue<T> | Array<TValue<T>> | Extract<T, undefined>;
 /** cast <T | T[]> as T[] */
 export type TArray<T> = Array<TValue<T>>;
