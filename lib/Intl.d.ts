4f980dfd585c360cb4193842231b5b019a1ec1b8
Michael <michael@lismore.lan>
2024-04-29 08:58:25 +1000

    ln lib

--- lib/Intl.d.ts
+++
@@ -1,9 +0,0 @@
-// TODO:  Temporary declaration file until TypeScript supports following Intl.Locale features
-
-declare namespace Intl {
-  // function getCanonicalLocales(locales: string | string[]): string[];
-
-  interface Locale extends LocaleOptions {
-    timeZones: string[];
-  }
-}
