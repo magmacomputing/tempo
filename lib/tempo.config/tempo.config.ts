c52296c7e6cd2f42f2279aef6a3dd67e753919bc
Michael McRae <michael@magmacomputing.com.au>
2026-02-12 15:41:05 +1100

    memoizeMethod

--- lib/tempo.config/tempo.config.ts
+++ lib/tempo.config/tempo.config.ts
@@ -1,6 +1,6 @@
 import { patBigInt, patRegExp } from '#core/shared/pattern.library.js';
+import { looseIndex } from '#core/shared/object.library.js';
 import type { Tempo } from '#core/shared/tempo.class.js';
-import type { Extend } from '#core/shared/type.library.js';
 
 // #region local const variables ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -8,17 +8,19 @@ import type { Extend } from '#core/shared/type.library.js';
 export const Match = {
 	/** string that looks like a BigInt */										bigint: patBigInt,
 	/** string that looks like a RegExp */										regexp: patRegExp,
-	/** match all {} pairs */																	braces: /{([^}]+)}/g,
+	/** match all {} pairs */																	braces: /{([\w]+)}/g,
+	/** named capture-group */																capture: /\?<([\w]+)>/g,
 	/** event */																							event: /^evt\d+$/,
 	/** period */																							period: /^per\d+$/,
 	/** two digit year */																			twoDigits: /^\d{2}$/,
 	/** hour-minute-second with no separator */								hhmiss: /(hh)(m[i|m])(ss)?/i,
 	/** separator characters (/ - . ,) */											separators: /[\/\-\.\s,]/,
 	/** modifier characters (+-<>=) */												modifiers: /[\+\-\<\>][\=]?|this|next|prev|first|last/,
+	/** post offset keywords (ago|hence) */										offsets: /ago|hence/,
 } as const
 
 /** Tempo Symbol registry */
-const $Token = {
+export const Token = looseIndex<string, symbol>()({
 	// Snippet Symbols ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	/** year */																								yy: Symbol('yy'),
 	/** month */																							mm: Symbol('mm'),
@@ -44,17 +46,22 @@ const $Token = {
 	/** weekday */																						wkd: Symbol('weekday'),
 	/** Tempo event */																				evt: Symbol('event'),
 	/** Tempo period */																				per: Symbol('period'),
-} as const
-export const Token = $Token as Extend<typeof $Token, string, symbol>
+})
 export type Token = typeof Token
 
 /**
  * user will need to know these in order to configure their own patterns  
- * Tempo.Snippet is a simple regex pattern		, e.g. { yy: /(\d{2})?\d{2})/ }    
- * Tempo.Layout is a string-combination of Snippet names	, e.g. '{yy}{sep}{mm}({sep}{dd})?{sfx}?'  
- * Tempo.Pattern converts a Layout into an anchored regex, used to parse a string | number in the Tempo constructor {DateTime} argument\
+ * Tempo.Snippet is a simple regex pattern object						, e.g. { Symbol('yy'): /((\d{2})?\d{2})/ }    
+ * Tempo.Layout is a string-combination of Snippet names		, e.g. '{yy}{sep}{mm}({sep}{dd})?{sfx}?'  
+ * Tempo.Pattern is a translation of a Layout/Snippets into an anchored regex.  
+ * The {pattern} is used to parse a string | number in the Tempo constructor {DateTime} argument
  */
-const $Snippet = {																					// define some snippets to help interpret input-strings
+
+/**
+ * a {snippet} is a simple regex pattern for a component of a date-time string (e.g. 'hour' or 'year').  
+*/
+// Note: computed Components ('dt', 'tm', 'evt', 'per') are added during 'Tempo.init()' (for static) and/or 'new Tempo()' (per instance)
+export const Snippet = looseIndex<symbol, RegExp>()({
 	[Token.yy]: /(?<yy>(\d{2})?\d{2})/,												// arbitrary upper-limit of yy=9999
 	[Token.mm]: /(?<mm>[0\s]?[1-9]|1[0-2]|Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?)/,	// month-name (abbrev or full) or month-number 01-12
 	[Token.dd]: /(?<dd>[0\s]?[1-9]|[12][0-9]|3[01])/,					// day-number 01-31
@@ -65,39 +72,32 @@ const $Snippet = {																					// define some snippets to help interpret
 	[Token.ff]: /(\.(?<ff>\d{1,9}))/,													// fractional-seconds up-to 9-digits
 	[Token.mer]: /(\s*(?<mer>am|pm))/,												// meridiem suffix (am,pm)
 	[Token.sfx]: /((?:[\s,T])({tm}))/,												// time-pattern suffix "T {tm}"
-	[Token.afx]: new RegExp(`((s)? (?<afx>ago|hence))?(?:${Match.separators.source})*`),// affix optional plural 's' and (ago|hence) to weekday
-	[Token.sep]: new RegExp(`(?<sep>${Match.separators.source})`),	// date-pattern separator character "/\\-., "
-	[Token.mod]: new RegExp(`((?<mod>${Match.modifiers.source})?(?<cnt>\\d*)\\s*)`),	// modifier (+,-,<,<=,>,>=) plus optional offset-count
-	// Note: computed Components ('dt', 'tm', 'evt', 'per', 'tzd') are added during 'Tempo.init()' (for static) and/or 'new Tempo()' (for instance)
-	// [Sym.dt]: new RegExp(''),																	// date and events
-	// [Sym.tm]: new RegExp(''),																	// time and periods
-	// [Sym.evt]: new RegExp(''),																// events
-	// [Sym.per]: new RegExp(''),																// periods
-	// [Sym.tzd]: new RegExp(''),																// time zone offset
-} as const
-export const Snippet = $Snippet as Extend<typeof $Snippet, symbol, RegExp>
+	[Token.tzd]: /(?<tzd>Z|(?:\+(?:(?:0\d|1[0-3]):?[0-5]\d|14:00)|-(?:(?:0\d|1[0-1]):?[0-5]\d|12:00)))/,	// time-zone offset	+14:00 to -12:00
+	[Token.afx]: new RegExp(`((s)? (?<afx>${Match.offsets.source}))?(?:${Match.separators.source})*`),		// affix optional plural 's' and (ago|hence)
+	[Token.mod]: new RegExp(`((?<mod>${Match.modifiers.source})?(?<cnt>\\d*)\\s*)`),											// modifier (+,-,<,<=,>,>=) plus optional offset-count
+	[Token.sep]: new RegExp(`(?:${Match.separators.source})`),// date-input separator character "/\\-., " (non-capture group)
+})
 export type Snippet = typeof Snippet
 
 /**
  * a {layout} is a Record of snippet-combinations describing an input DateTime argument  
  * the Layout's keys are in the order that they will be checked against an input value  
  */
-const $Layout = {
-	[Token.dt]: '{dd}{sep}?{mm}({sep}?{yy})?|{mod}?({evt})',		// calendar or event
-	[Token.tm]: '{hh}{mi}?{ss}?{ff}?{mer}?|{per}',							// clock or period 
-	[Token.wkd]: '{mod}?{wkd}{afx}?{sfx}?',											// special layout (no {dt}!) used for weekday calcs (only one that requires {wkd} pattern)
-	[Token.dtm]: '({dt}){sfx}?',																// calendar/event and clock/period
-	[Token.dmy]: '{wkd}?{dd}{sep}?{mm}({sep}{yy})?{sfx}?',			// day-month(-year)
-	[Token.mdy]: '{wkd}?{mm}{sep}?{dd}({sep}{yy})?{sfx}?',			// month-day(-year)
-	[Token.ymd]: '{wkd}?{yy}{sep}?{mm}({sep}{dd})?{sfx}?',			// year-month(-day)
-	[Token.evt]: '{evt}',																				// event component only
-	[Token.per]: '{per}',																				// period component only
-} as const
-export const Layout = $Layout as Extend<typeof $Layout, symbol, string>
+export const Layout = looseIndex<symbol, string>()({
+	[Token.dt]: '{dd}{sep}?{mm}({sep}?{yy})?|{mod}?({evt})',	// calendar or event
+	[Token.tm]: '({hh}{mi}?{ss}?{ff}?{mer}?|{per})',					// clock or period
+	[Token.wkd]: '{mod}?{wkd}{afx}?{sfx}?',										// special layout (no {dt}!) used for weekday calcs (only one that requires {wkd} pattern)
+	[Token.dtm]: '({dt}){sfx}?',															// calendar/event and clock/period
+	[Token.dmy]: '{wkd}?{dd}{sep}?{mm}({sep}{yy})?{sfx}?',		// day-month(-year)
+	[Token.mdy]: '{wkd}?{mm}{sep}?{dd}({sep}{yy})?{sfx}?',		// month-day(-year)
+	[Token.ymd]: '{wkd}?{yy}{sep}?{mm}({sep}{dd})?{sfx}?',		// year-month(-day)
+	[Token.evt]: '{evt}',																			// event component only
+	[Token.per]: '{per}',																			// period component only
+})
 export type Layout = typeof Layout
 
 /** an {event} is a Record of regex-pattern-like keys that describe pre-defined Date strings */
-const $Event = {
+export const Event = looseIndex<string, string>()({
 	'new.?years? ?eve': '31 Dec',
 	'nye': '31 Dec',
 	'new.?years?( ?day)?': '01 Jan',
@@ -106,12 +106,11 @@ const $Event = {
 	'christmas': '25 Dec',
 	'xmas ?eve': '24 Dec',
 	'xmas': '25 Dec',
-} as const
-export const Event = $Event as Extend<typeof $Event, string, string>
+})
 export type Event = typeof Event
 
 /** a {period} is a Record of regex-pattern-like keys that describe pre-defined Time strings */
-const $Period = {
+export const Period = looseIndex<string, string>()({
 	'mid[ -]?night': '24:00',
 	'morning': '8:00',
 	'mid[ -]?morning': '10:00',
@@ -120,8 +119,7 @@ const $Period = {
 	'after[ -]?noon': '3:00pm',
 	'evening': '18:00',
 	'night': '20:00',
-} as const
-export const Period = $Period as Extend<typeof $Period, string, string>
+})
 export type Period = typeof Period
 
 /** Reasonable default options for initial Tempo config */
