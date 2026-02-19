f1072dc4476ca5622b3aebd35783ee7e8502a717
Michael McRae <michael@magmacomputing.com.au>
2026-02-20 07:43:40 +1100

    test

--- test/tempo_regexp.test.ts
+++ test/tempo_regexp.test.ts
@@ -1,5 +1,5 @@
 import { Tempo } from '../lib/tempo.class.js';
-import { Token } from '../lib/tempo.config/tempo.config.js';
+import { Token } from '../lib/tempo.config/tempo.defaults.js';
 
 describe('Tempo.regexp', () => {
   test('should expand snippets and handle nested named capture groups', () => {
