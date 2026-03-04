e23f371468dab9ecf5738bab67bdcb61160db14b
Michael McRae <michael@magmacomputing.com.au>
2026-03-04 15:51:35 +1100

    Immutable decorator cleanup

--- lib/class.library.ts
+++ lib/class.library.ts
@@ -2,7 +2,7 @@ import { Registry } from '#core/shared/serialize.library.js';
 import type { Constructor } from '#core/shared/type.library.js';
 
 /**
- * Some interesting Decorators
+ * Some interesting Class Decorators
  */
 
 /** decorator to freeze a Class to prevent modification */
@@ -11,18 +11,23 @@ export function Immutable<T extends Constructor>(value: T, { kind, name, addInit
 
 	switch (kind) {
 		case 'class':
-			addInitializer(() => {																// wait for construction to complete
-				Object.freeze(value);																// freeze the class instance
-				Object.freeze(value.prototype);											// freeze the class prototype
-			})
-
-			return new Proxy(value, {
-				construct: function (source, argumentsList, target) {
-					const instance = Reflect.construct(source, argumentsList, target);
-					Object.freeze(instance);													// freeze the instance
-					return instance;
+			const wrapper = {
+				[name]: class extends value {												// anonymous class infers name from property
+					constructor(...args: any[]) {
+						super(...args);
+						Object.freeze(this);														// freeze the instance
+					}
 				}
-			}) as T;
+			}[name] as T;
+
+			addInitializer(() => {																// wait for construction to complete
+				Object.freeze(value);																// freeze the static properties
+				Object.freeze(value.prototype);											// freeze the prototype methods
+				Object.freeze(wrapper);															// freeze the wrapper static properties
+				Object.freeze(wrapper.prototype);										// freeze the wrapper prototype methods
+			});
+
+			return wrapper;
 
 		default:
 			throw new Error(`@Immutable decorating unknown 'kind': ${kind} (${name})`);
@@ -35,7 +40,7 @@ export function Serializable<T extends Constructor>(value: T, { kind, name, addI
 
 	switch (kind) {
 		case 'class':
-			addInitializer(() => Registry.set(`$${name}`, value));// register the class for serialization
+			addInitializer(() => Registry.set(`$${name}`, value));// register the class for serialization, via its toString() method
 
 			return value;
 
@@ -50,12 +55,16 @@ export function Static<T extends Constructor>(value: T, { kind, name }: ClassDec
 
 	switch (kind) {
 		case 'class':
-			return class extends (value as any) {
-				constructor(...args: any[]) {
-					super(...args);
-					throw new TypeError(`${name} is not a constructor`);
+			const wrapper = {
+				[name]: class extends value {
+					constructor(...args: any[]) {
+						super(...args);
+						throw new TypeError(`${name} is not a constructor`);
+					}
 				}
-			} as T;
+			}[name] as T;
+
+			return wrapper;
 
 		default:
 			throw new Error(`@Static decorating unknown 'kind': ${kind} (${name})`);
