c416b88d69005b3e998ad8db7fa6f45faa8e9e6a
Michael McRae <michael@magmacomputing.com.au>
2026-03-24 21:24:28 +1100

    Delete packages/tempo/.rollup.cache/home/michael/Project/tempo/packages/tempo/src directory

--- packages/tempo/.rollup.cache/home/michael/Project/tempo/packages/tempo/src/index.js
+++
@@ -1,6 +0,0 @@
-export * from './tempo.class.js'; // Temporal wrapper
-export * as enums from './tempo.config/tempo.enum.js';
-export * from '#library/pledge.class.js'; // Promise wrapper
-export * from '#library/serialize.library.js'; // stringify, objectify, cloneify functions
-export * from '#library/enumerate.library.js'; // enumify function
-//# sourceMappingURL=index.js.map
--- packages/tempo/.rollup.cache/home/michael/Project/tempo/packages/tempo/src/tempo.config/terms/term.season.d.ts
+++
@@ -1,7 +0,0 @@
-import { type Range } from './term.utils.js';
-import type { Tempo } from '#tempo/tempo.class.js';
-export declare const key = "szn";
-export declare const scope = "season";
-export declare const description = "Meteorlogical season";
-/** determine where the current Tempo instance fits within the above range */
-export declare function define(this: Tempo, keyOnly?: boolean): PropertyKey | Range | undefined;
